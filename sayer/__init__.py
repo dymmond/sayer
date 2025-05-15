32e48ac163b911fb54bd9de0b56d11cb2147c6ed
tarsil <tiago.arasilva@gmail.com>
2025-05-15 14:33:47 +0200

    Add SayerConfig

--- sayer/__init__.py
+++ sayer/__init__.py
@@ -1,3 +1,4 @@
+from sayer.config import get_config
 from sayer.core import command, group
 from sayer.loader import load_commands_from
 from sayer.runner import run
@@ -5,4 +6,4 @@ from sayer.ui_helpers import confirm, progress, table
 
 __version__ = "0.1.0"
 
-__all__ = ["command", "group", "run", "load_commands_from", "confirm", "progress", "table"]
+__all__ = ["command", "group", "run", "load_commands_from", "confirm", "progress", "table", "get_config"]

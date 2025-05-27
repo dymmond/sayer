e8f4aea6fd1bdb2f39b719ada1689dce03b1ea27
tarsil <tiago.arasilva@gmail.com>
2025-05-27 15:39:51 +0200

    Add testing errors

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -10,6 +10,6 @@ def test_raise_value_error_on_default_with_nargs():
     Test that a ValueError is raised when a command with nargs is defined with a default value.
     """
     with pytest.raises(ValueError):
+
         @command
-        def foo(arg: Annotated[str, Argument(nargs=-1, default="default")]):
-            ...
+        def foo(arg: Annotated[str, Argument(nargs=-1, default="default")]): ...
